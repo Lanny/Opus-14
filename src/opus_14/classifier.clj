0c7dd37007c4287d80c0b1bc00c85035f97b46da
RyanJenkins <rjenkins@mail.sfsu.edu>
2014-09-03 18:44:11 -0700

    Added tokenize function.

--- src/opus_14/classifier.clj
+++ src/opus_14/classifier.clj
@@ -1,8 +1,29 @@
 (ns opus-14.classifier
   "A simple naive bayes classifier and word based feature extractor."
   (:require
-    (clojure [set :as sets])
+    (clojure [set :as sets]
+             [string :as string])
     [swiss.arrows :refer :all]))
+
+(def stop-words
+  #{"" "a" "about" "above" "after" "again" "against" "all" "am" "an" "and"
+    "any" "are" "aren't" "as" "at" "be" "because" "been" "before" "being"
+    "below" "between" "both" "but" "by" "can't" "cannot" "could" "couldn't"
+    "did" "didn't" "do" "does" "doesn't" "doing" "don't" "down" "during" "each"
+    "few" "for" "from" "further" "had" "hadn't" "has" "hasn't" "have" "haven't"
+    "having" "he" "he'd" "he'll" "he's" "her" "here" "here's" "hers" "herself"
+    "him" "himself" "his" "how" "how's" "i" "i'd" "i'll" "i'm" "i've" "if" "in"
+    "into" "is" "isn't" "it" "it's" "its" "itself" "let's" "me" "more" "most"
+    "mustn't" "my" "myself" "no" "nor" "not" "of" "off" "on" "once" "only" "or"
+    "other" "ought" "our" "oursourselves" "out" "over" "own" "same" "shan't"
+    "she" "she'd" "she'll" "she's" "should" "shouldn't" "so" "some" "such"
+    "than" "that" "that's" "the" "their" "theirs" "them" "themselves" "then"
+    "there" "there's" "these" "they" "they'd" "they'll" "they're" "they've"
+    "this" "those" "through" "to" "too" "under" "until" "up" "very" "was"
+    "wasn't" "we" "we'd" "we'll" "we're" "we've" "were" "weren't" "what"
+    "what's" "when" "when's" "where" "where's" "which" "while" "who" "who's"
+    "whom" "why" "why's" "with" "won't" "would" "wouldn't" "you" "you'd"
+    "you'll" "you're" "you've" "your" "yours" "yourself" "yourselves"})
 
 (defn normalize
   "Given a map m with all numeric values, return a map n where the sum of
@@ -15,6 +36,16 @@
         ; map back I guess
       (into {} (for [[k v] m]
                  [k (/ v denom)])))))
+
+(defn tokenize
+  "A primitive word-wise tokenization algorithm."
+  [s]
+  (-<>> s
+        (string/lower-case)
+        (string/split <> #"\s+")
+        (map #(second (re-find #"^['\"]*(.+?)[.,?!'\"]*$" %)))
+        (filter #(not (nil? %)))
+        (filter #(not (contains? stop-words %)))))
 
 (defn make-classifier
   "Returns an untrained classifier with `klasses` as the potential classes"
